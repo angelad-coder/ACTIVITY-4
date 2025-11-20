9b1f7731b11c2deabf975ad292d7341d7811b10d
angelad-coder <gelaandal05@gmail.com>
2025-11-20 18:32:11 +0800

    feat/list-species-catered

--- veterinary.sql
+++ veterinary.sql
@@ -143,3 +143,6 @@ WHERE appointid IN (SELECT appointid FROM appointments WHERE animalid = 2);
 UPDATE doctors
 SET dlastname = 'Reyes-Gonzales'
 WHERE doctorid = 4;
+
+SELECT DISTINCT species
+FROM animals;
